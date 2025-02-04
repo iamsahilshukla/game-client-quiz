966155f21f023a846ddd581fd596db2afda10972
Nostradamus <nostradamus@Nostradamuss-MacBook-Pro.local>
2025-02-05 02:26:07 +0530

    fix build issue

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -2,6 +2,8 @@ import { io } from 'socket.io-client'; // Import the socket.io client library
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:9000/';
+  process.env.NODE_ENV === 'production'
+    ? 'https://donkeyquizgame.onrender.com/'
+    : 'https://donkeyquizgame.onrender.com/';
 
 export const socket = io(URL, { transports: ['websocket'] });
